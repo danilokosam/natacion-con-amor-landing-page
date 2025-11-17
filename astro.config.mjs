0bbabab191d54827175ad690630eaed72526bc8b
Daniel Duarte <danielduartemora27@gmail.com>
2025-11-17 18:38:12 +0100

    chore: initial commit with Astro + Tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com", // Replace later.
+  integrations: [preact(), icon(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
