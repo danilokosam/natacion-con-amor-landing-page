ac8e3a5ae56d3de2766519f088da2a6c152ec0eb
Yoan <unlock.eagle@gmail.com>
2025-11-19 20:47:43 +0100

    fix(allStaticData): static data for instagram & youtube url

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,31 @@
-//* comento esta parte para inicializar el proyecto sin errores
+import { defineCollection, z } from "astro:content";
 
-// import { defineCollection, z } from "astro:content";
+const jsonDataCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    //Define JSON-file structure
+    profileImage: z.string(),
+    profileAlt: z.string(),
+    profileLink: z.string(),
+    profileTitle: z.string(),
+    profileName: z.string(),
+    email: z.string().email(),
+    instagram: z.string().url(),
+    youtube: z.string().url(),
+    alias: z.string(),
+    contactSectionTitle: z.string(),
+    contactSectionSubtitle: z.string(),
+    contactSectionButtonText: z.string(),
+    contactSectionButtonIcon: z.string(),
+    instagramIconName: z.string(),
+    youtubeIconName: z.string(),
+    emailIconName: z.string(),
+    hobbies: z.array(z.string()),
+    pageTitle: z.string(),
+    pageDescription: z.string(),
+  }),
+});
 
-// const jsonDataCollection = defineCollection({
-//   type: "data",
-//   schema: z.object({
-//     //Define JSON-file structure
-//     profileImage: z.string(),
-//     profileAlt: z.string(),
-//     profileLink: z.string(),
-//     profileTitle: z.string(),
-//     profileName: z.string(),
-//     email: z.string().email(),
-//     instagram: z.string().url(),
-//     youtube: z.string().url(),
-//     alias: z.string(),
-//     contactSectionTitle: z.string(),
-//     contactSectionSubtitle: z.string(),
-//     contactSectionButtonText: z.string(),
-//     contactSectionButtonIcon: z.string(),
-//     instagramIconName: z.string(),
-//     youtubeIconName: z.string(),
-//     emailIconName: z.string(),
-//     hobbies: z.array(z.string()),
-//     pageTitle: z.string(),
-//     pageDescription: z.string(),
-//   }),
-// });
-
-// export const collections = {
-//   staticData: jsonDataCollection,
-// };
+export const collections = {
+  staticData: jsonDataCollection,
+};
