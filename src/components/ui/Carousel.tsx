e7f61aa86d0a051142865b6c9f30d106dd043f56
Daniel Duarte <danielduartemora27@gmail.com>
2025-12-13 12:14:02 +0100

    Realizamos cambios en la barra de navegación dandole un toque de estilo transparente a su fondo, agregamos un margin interior en el carousel y cambiamos el scale del hover en los botones del componente Social.astro

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -55,7 +55,7 @@ const Carousel = () => {
         }
       `}</style>
       <div
-        className='relative mx-auto w-full max-w-full overflow-hidden'
+        className='relative mx-auto w-full max-w-full overflow-hidden mb-16'
         onMouseEnter={() => setStopScroll(true)}
         onMouseLeave={() => setStopScroll(false)}
       >
